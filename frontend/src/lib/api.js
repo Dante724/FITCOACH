a29234c896161ea866210eecf6faef2b37ead9fc
Aryan <59132949+Dante724@users.noreply.github.com>
2026-08-01 09:08:27 -0400

    api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const BACKEND_URL = process.env.https://api.thefitcoach.in
+;
 export const API = `${BACKEND_URL}/api`;
 
 export const api = axios.create({
